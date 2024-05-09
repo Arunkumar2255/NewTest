d2e91d881492c547f71a598e4264a64d4ba87570
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-05-09 14:06:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,4 @@ provider  "aws" {
 resource "aws_instance" "Linux" {
     ami            =  "ami-07caf09b362be10b8"
     instance_type  =  "t2.micro"
-    key_name       =   "NV"
-    tags  = {
-        Name  = "Demo_server"
-}
 }
