d9039a2f95ceb76ccf8a78e68e531077d0e6fd63
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-05-09 14:22:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-terraform 
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.2.0"
     }
  }
+}
 provider  "aws" {
     region         = "us-east-1"
 }
