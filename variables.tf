9c13ae6cf84b2c6581412a0b4dde1380c9d9d7bf
Arunkumar2255 <148421914+Arunkumar2255@users.noreply.github.com>
2024-05-16 11:44:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,3 +30,15 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "The AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "The AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
